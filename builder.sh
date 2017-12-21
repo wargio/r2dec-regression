40294cb094fafd4b6538e118b896ca7d928ed582
wargio <wargio@libero.it>
2017-12-21 13:58:01 +0100

    added arch to filename

--- builder.sh
+++ builder.sh
@@ -18,12 +18,13 @@ echo "  Worker output:"
 while read LINE; do
 	NAME=$(basename $LINE)
 	echo "    building: $TESTFOLDER/$NAME.json"
-	r2 -Q -c "aaa; s main; e asm.arch > $TMPFOLDER/arch.txt; agj > $TMPFOLDER/agj.json; isj > $TMPFOLDER/isj.json; izj > $TMPFOLDER/izj.json; #!pipe r2dec > $TESTFOLDER/$NAME.output.txt" "$LINE" 2> $TMPFOLDER/stderr.txt
+	r2 -Q -c "aaa; s main; e asm.arch > $TMPFOLDER/arch.txt; agj > $TMPFOLDER/agj.json; isj > $TMPFOLDER/isj.json; izj > $TMPFOLDER/izj.json; #!pipe r2dec > $TMPFOLDER/output.txt" "$LINE" 2> $TMPFOLDER/stderr.txt
 	ARCH=$(cat "$TMPFOLDER/arch.txt")
 	AGJ=$(cat "$TMPFOLDER/agj.json")
 	ISJ=$(cat "$TMPFOLDER/isj.json")
 	IZJ=$(cat "$TMPFOLDER/izj.json")
-	echo '{"name":"'$NAME'","arch":"'$ARCH'","agj":'$AGJ',"isj":'$ISJ',"izj":'$IZJ'}' > "$TESTFOLDER/$NAME.json"
+	echo '{"name":"'$NAME'","arch":"'$ARCH'","agj":'$AGJ',"isj":'$ISJ',"izj":'$IZJ'}' > "$TESTFOLDER/$NAME.$ARCH.json"
+	mv "$TMPFOLDER/output.txt" "$TESTFOLDER/$NAME.$ARCH.output.txt"
 done < $FILENAME
 
 if [ ! "$TMPFOLDER" == "/tmp" ]; then
