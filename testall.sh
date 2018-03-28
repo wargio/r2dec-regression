5feb6ebbe8c221c9188ebfb23778a98aaa4387b9
wargio <wargio@libero.it>
2018-03-28 14:32:12 +0200

    fixed bad while

--- testall.sh
+++ testall.sh
@@ -4,12 +4,12 @@ TESTFOLDER=./tests
 TMPFOLDER=./tmp/
 R2DECFOLDER=$1
 TRAVIS=$2
-RMCMD="rmdir"
+RMCMD="rm -rf"
 ERROR=false
 DIFF="diff -u"
 
-if [ ! -z "$TRAVIS" ]; then
-	RMCMD="rm -rf"
+if [ -z "$TRAVIS" ]; then
+	RMCMD="rmdir"
 	DIFF="diff --color=always -u"
 fi
 
@@ -32,9 +32,9 @@ for ELEM in $TESTS; do
 	NAME=$(basename "$ELEM")
 	OUTPUTFILE="$TMPFOLDER/$NAME.output.txt"
 	$R2DECBINFLD/r2dec-test "$R2DECFOLDER" "$ELEM.json" > "$OUTPUTFILE" || break
-	DIFF=$(diff -u "$ELEM.output.txt" "$OUTPUTFILE")
+	DIFFOUTPUT=$(diff -u "$ELEM.output.txt" "$OUTPUTFILE")
 
-	if [ ! -z "$DIFF" ]; then
+	if [ ! -z "$DIFFOUTPUT" ]; then
 		echo "[XX]: $NAME"
 		$DIFF "$ELEM.output.txt" "$OUTPUTFILE"
 		ERROR=true
@@ -49,12 +49,14 @@ if [ ! "$TMPFOLDER" == "/tmp" ] ; then
 	$RMCMD "$TMPFOLDER"
 fi
 
+
+if $ERROR; then
+	exit 1;
+fi
+
 if [ -f "$R2DECBINFLD/r2dec-test" ]; then
 	echo "cleaning binary folder"
     make --no-print-directory clean -C "$R2DECBINFLD"
 fi
 
-if $ERROR; then
-	exit 1;
-fi
 exit 0;
