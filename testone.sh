84994f7ee364492cb93c6c02fb51d4daa9da7c7b
wargio <wargio@libero.it>
2018-07-11 17:01:53 +0200

    updated builder and test tool

--- testone.sh
+++ testone.sh
@@ -18,19 +18,27 @@ if [ ! -f "$R2DECBINFLD/r2dec-test" ]; then
 	echo "building binary src"
     make --no-print-directory testbin -C "$R2DECBINFLD"
 fi
-
-ELEM=$(find "$TESTFOLDER" | grep "$TESTNAME" | grep ".json" | sed "s/.json//g")
-
+ELEM=$(find "$TESTFOLDER" | grep "$TESTFOLDER/$TESTNAME" | grep ".json" | sed "s/.json//g")
+echo "$ELEM"
 mkdir "$TMPFOLDER"
 
 NAME=$(basename "$ELEM")
+if [ -z "$NAME" ]; then
+	echo "Empty name.."
+	exit 1;
+fi
+
 OUTPUTFILE="$TMPFOLDER/$NAME.output.txt"
 $R2DECBINFLD/r2dec-test "$R2DECFOLDER" "$ELEM.json" > "$OUTPUTFILE" || break
 DIFFOUTPUT=$(diff -u "$ELEM.output.txt" "$OUTPUTFILE")
 
 if [ ! -z "$DIFFOUTPUT" ]; then
 	echo "[XX]: $NAME"
-	$DIFF "$ELEM.output.txt" "$OUTPUTFILE"
+	if [ -f "$ELEM.output.txt" ]; then
+		$DIFF "$ELEM.output.txt" "$OUTPUTFILE"
+	else
+		cat "$OUTPUTFILE"
+	fi
 	ERROR=true
 else
 	echo "[OK]: $NAME"
--- tools/make_test.sh
+++ tools/make_test.sh
@@ -7,14 +7,13 @@ TMPFOLDER="$3"
 if [ $# -eq 3 ]; then
 	NAME=$(basename $FILENAME)
 	echo "    building: $TESTFOLDER/$NAME.json"
-	r2 -A -Q -c "s main; e asm.arch > $TMPFOLDER/arch.txt; agj > $TMPFOLDER/agj.json; isj > $TMPFOLDER/isj.json; izj > $TMPFOLDER/izj.json; aflj > $TMPFOLDER/aflj.json; #!pipe r2dec > $TMPFOLDER/output.txt" "$FILENAME" 2> $TMPFOLDER/stderr.txt
-	ARCH=$(cat "$TMPFOLDER/arch.txt")
-	AGJ=$(cat "$TMPFOLDER/agj.json")
-	ISJ=$(cat "$TMPFOLDER/isj.json")
-	IZJ=$(cat "$TMPFOLDER/izj.json")
-	AFLJ=$(cat "$TMPFOLDER/aflj.json")
-	echo '{"name":"'$NAME'","arch":"'$ARCH'","agj":'$AGJ',"isj":'$ISJ',"izj":'$IZJ',"aflj":'$AFLJ'}' > "$TESTFOLDER/$NAME.$ARCH.json"
-	mv "$TMPFOLDER/output.txt" "$TESTFOLDER/$NAME.$ARCH.output.txt"
+	ARCH=$(r2 -Q -c "e asm.arch" "$FILENAME")
+	FILEJSON="$TESTFOLDER/$NAME.$ARCH.json"
+	FILEOUT="$TESTFOLDER/$NAME.$ARCH.output.txt"
+	r2 -A -Q -c "s main; pddi" "$FILENAME" > "$FILEJSON" 2> "$TMPFOLDER/stderr.txt"
+	if [ ! -f "$FILEOUT" ]; then
+		mv "$TMPFOLDER/output.txt" "$FILEOUT"
+	fi
 else
 	echo "make_test.sh <file> <test folder> <tmp folder>"
 fi
